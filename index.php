051a240f114b641ccef661240d4a13cd51531b9e
keanedawg <cameronfife@hotmail.com>
2018-01-27 13:05:37 -0700

    added some products

--- index.php
+++ index.php
@@ -8,10 +8,25 @@
 <?php
   echo "hello world";
 ?>
+<h1>Useless Junk</h1>
 
-<div>
-<h1>
 
+<div class="product">
+<h2>Rag</h2>
+<h3>$1.50</h3>
+<button>add to cart</button>
+</div>
+
+<div class="product">
+<h2>Log</h2>
+<h3>$4.50</h3>
+<button>add to cart</button>
+</div>
+
+<div class="product">
+<h2>Burned Out Lightbulb</h2>
+<h3>$0.50</h3>
+<button>add to cart</button>
 </div>
 
 </body>
