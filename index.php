6a3d66350ba0027df1a42ce54abec83e14ade7e5
keanedawg <cameronfife@hotmail.com>
2018-01-27 12:58:19 -0700

    Added css

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<html>
+<header>
+  <link rel="stylesheet" type="text/css" href="snazzy.css">
+
+
+</header>
+<body>
 <?php
   echo "hello world";
 ?>
+
+<div>
+<h1>
+
+</div>
+
+</body>
+
+</html>
