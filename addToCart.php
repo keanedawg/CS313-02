e04bc31cd0bf6babd5aba42b4c994fa903e2fda9
keanedawg <cameronfife@hotmail.com>
2018-01-27 14:29:46 -0700

    Adds multiple items to array

--- addToCart.php
+++ addToCart.php
@@ -6,8 +6,9 @@
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+
 if (isset($_SESSION['myproducts'])) {
-    echo "not empty";
+    $_SESSION['myproducts'][] = $_GET['product'];
 }
 else {
     $_SESSION['myproducts'] = array();
@@ -15,7 +16,4 @@ else {
 }
 
 print_r($_SESSION['myproducts']);
-
-session_destroy();
-
 ?>
