ce2a37aafc7a441e2a2bbf877845ee2aa0f08442
keanedawg <cameronfife@hotmail.com>
2018-01-27 13:23:33 -0700

    Added all php pages and header php template

--- browse.php
+++ browse.php
@@ -4,14 +4,8 @@
 // Start the session
 session_start();
 header("Refresh:5");
+include('header.php')
 ?>
-<html>
-<header>
-  <link rel="stylesheet" type="text/css" href="snazzy.css">
-
-
-</header>
-<body>
 <?php
   echo "hello world";
 ?>
---
+++ header.php
@@ -0,0 +1,9 @@
+<?php
+echo '<html> 
+<header> 
+  <link rel="stylesheet" type="text/css" href="snazzy.css"> 
+
+
+</header>
+<body>';
+?>
